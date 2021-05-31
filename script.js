4813f89f67569124b35b63b9e1c6735d519b2233
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-05-31 21:48:16 +0700

    Benerin index.html

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $("#arrowleft").on("click", function () {
     console.log("Click Left");
-    var currentDiv = $(".middle-container-items.active");
+    var currentDiv = $(".middle-container-item.active");
     var prevDiv = currentDiv.prev();
 
-    if (prevDiv.length) {
+    if (prevDiv.length > 3) {
       currentDiv.removeClass("active").css("z-index", -10);
       prevDiv.addClass("active").css("z-index", 10);
     }
@@ -14,10 +14,10 @@ $(document).ready(function () {
 $(document).ready(function () {
   $("#arrowright").on("click", function () {
     console.log("Click Right");
-    var currentDiv = $(".middle-container-items.active");
+    var currentDiv = $(".middle-container-item.active");
     var nextDiv = currentDiv.next();
 
-    if (nextDiv.length) {
+    if (nextDiv.length > 3) {
       currentDiv.removeClass("active").css("z-index", -10);
       nextDiv.addClass("active").css("z-index", 10);
     }
