eb14278175e712f62e878e057c648cd6f02ee784
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-06-01 07:44:05 +0700

    tambah product page + Assets

---
+++ productScript.js
@@ -0,0 +1,29 @@
+function setImage(path){
+    document.getElementById('showImage').src = path;
+}
+
+$(document).ready(function () {
+    $("#arrowleft").on("click", function () {
+      console.log("Click Left");
+      var currentDiv = $(".image.active");
+      var prevDiv = currentDiv.prev();
+  
+      if (prevDiv.length > 3) {
+        currentDiv.removeClass("active").css("z-index", -10);
+        prevDiv.addClass("active").css("z-index", 10);
+      }
+    });
+  });
+  
+  $(document).ready(function () {
+    $("#arrowright").on("click", function () {
+      console.log("Click Right");
+      var currentDiv = $(".image.active");
+      var nextDiv = currentDiv.next();
+  
+      if (nextDiv.length > 3) {
+        currentDiv.removeClass("active").css("z-index", -10);
+        nextDiv.addClass("active").css("z-index", 10);
+      }
+    });
+  });
