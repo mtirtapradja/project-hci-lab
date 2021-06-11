d28b60483770fe6cf9c291b51df419abab9fd559
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-06-11 12:05:49 +0700

    benerin animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
   let fields = $('.middle-container-item');
-  let media4 = window.matchMedia('(max-width: 1800px)');
-  let media3 = window.matchMedia('(max-width: 1300px)');
-  let media2 = window.matchMedia('(max-width: 900px)');
-  let media1 = window.matchMedia('(max-width: 400px)');
+  let media3 = window.matchMedia('(max-width: 1500px)');
+  let media2 = window.matchMedia('(max-width: 1100px)');
+  let media1 = window.matchMedia('(max-width: 800px)');
 
   let currentIndex = 0;
   let targetIndex = 4;
@@ -14,18 +13,21 @@ $(document).ready(function(){
 
   media3.addListener(function(){
     if(media3.matches){
+      fields.css('height','20em');
       targetIndex -= 1;
     }else{
-      fields.eq(targetIndex).css('opacity','1');
-      fields.eq(targetIndex).css('left','0%');
-      fields.eq(targetIndex).show();
-      targetIndex+=1;
       if(targetIndex===fields.length){
+        currentIndex -=1;
         fields.eq(currentIndex).css('opacity','1');
         fields.eq(currentIndex).css('left','0%');
-        fields.eq(currentIndex).show();
-        currentIndex -=1;
+        fields.eq(currentIndex).show();  
+      }else{
+        fields.eq(targetIndex).css('opacity','1');
+        fields.eq(targetIndex).css('left','0%');
+        fields.eq(targetIndex).show();
+        targetIndex+=1;
       }
+      fields.css('height','23em');
     }
     for(i=targetIndex ; i<fields.length ; i++){
       fields.eq(i).hide();
@@ -34,12 +36,21 @@ $(document).ready(function(){
 
   media2.addListener(function(){
     if(media2.matches){
+      fields.css('height','17em');
       targetIndex -= 1;
     }else{
-      fields.eq(targetIndex).css('opacity','1');
-      fields.eq(targetIndex).css('left','0%');
-      fields.eq(targetIndex).show();
-      targetIndex += 1;
+      if(targetIndex===fields.length){
+        currentIndex -=1;
+        fields.eq(currentIndex).css('opacity','1');
+        fields.eq(currentIndex).css('left','0%');
+        fields.eq(currentIndex).show();
+      }else{
+        fields.eq(targetIndex).css('opacity','1');
+        fields.eq(targetIndex).css('left','0%');
+        fields.eq(targetIndex).show();
+        targetIndex += 1;
+      }
+      fields.css('height','20em');
     }
     for(i=targetIndex ; i<fields.length ; i++){
       fields.eq(i).hide();
@@ -48,19 +59,26 @@ $(document).ready(function(){
 
   media1.addListener(function(){
     if(media1.matches){
+      fields.css('height','13em');
       targetIndex -= 1;
     }else{
-      fields.eq(targetIndex).css('opacity','1');
-      fields.eq(targetIndex).css('left','0%');
-      fields.eq(targetIndex).show();
-      targetIndex += 1;
+      if(targetIndex===fields.length){
+        currentIndex -=1;
+        fields.eq(currentIndex).css('opacity','1');
+        fields.eq(currentIndex).css('left','0%');
+        fields.eq(currentIndex).show();
+      }else{
+        fields.eq(targetIndex).css('opacity','1');
+        fields.eq(targetIndex).css('left','0%');
+        fields.eq(targetIndex).show();
+        targetIndex += 1;
+      }
+      fields.css('height','17em');
     }
     for(i=targetIndex ; i<fields.length ; i++){
       fields.eq(i).hide();
     }
   });
-
-
 
   $('#arrowleft1').on('click',function(){
     if(currentIndex>0){
@@ -90,7 +108,7 @@ $(document).ready(function(){
                 left: '0%',
                 opacity: 1
               },500);
-            })
+            });
            }); 
         });
       });
@@ -125,7 +143,7 @@ $(document).ready(function(){
                 left: '0%',
                 opacity: 1
               },500);
-            })
+            });
            }); 
         });
       });
@@ -157,7 +175,7 @@ $(document).ready(function(){
               left: '0%',
               opacity: 1
             },500);
-          })
+          });
         }); 
       });
     }
@@ -188,12 +206,111 @@ $(document).ready(function(){
               left: '0%',
               opacity: 1
             },500);
-          })
+          });
         }); 
       });
     }
   });
 
+  $('#arrowleft3').on('click',function(){
+    if(currentIndex>0){
+      fields.eq(targetIndex-1).animate({
+        left: '50%',
+        opacity: 0
+      },500,function(){
+        fields.eq(targetIndex-2).animate({
+          left: '50%'
+        },500,function(){
+          fields.eq(targetIndex-1).hide();
+          for(j=currentIndex-1 ; j<targetIndex-1 ; j++){
+            fields.eq(j).css('left','0%');
+          }
+          currentIndex -=1;
+          targetIndex -=1;
+          fields.eq(currentIndex).show();
+          fields.eq(currentIndex).css('left','-50%');
+          fields.eq(currentIndex).css('opacity','0');
+          fields.eq(currentIndex).animate({
+            left: '0%',
+            opacity: 1
+          },500);
+        });
+      }); 
+    }
+  });
+
+  $('#arrowright3').on('click',function(){
+    if(targetIndex+1<=fields.length){
+      fields.eq(currentIndex).animate({
+        left: '-50%',
+        opacity: 0
+      },500,function(){
+        fields.eq(currentIndex+1).animate({
+          left: '-50%'
+        },500,function(){
+            fields.eq(currentIndex).hide();
+            for(j=currentIndex+1 ; j<targetIndex ; j++){
+              fields.eq(j).css('left','0%');
+            }
+            currentIndex +=1;
+            targetIndex +=1;
+            fields.eq(targetIndex-1).show();
+            fields.eq(targetIndex-1).css('left','50%');
+            fields.eq(targetIndex-1).css('opacity','0');
+            fields.eq(targetIndex-1).animate({
+              left: '0%',
+              opacity: 1
+            },500);
+          }); 
+      });
+    }
+  });
+
+  $('#arrowleft4').on('click',function(){
+    if(currentIndex>0){
+      fields.eq(targetIndex-1).animate({
+        left: '100%',
+        opacity: 0
+      },500,function(){
+        fields.eq(targetIndex-1).hide();
+        for(j=currentIndex-1 ; j<targetIndex-1 ; j++){
+          fields.eq(j).css('left','0%');
+        }
+        currentIndex -=1;
+        targetIndex -=1;
+        fields.eq(currentIndex).show();
+        fields.eq(currentIndex).css('left','-100%');
+        fields.eq(currentIndex).css('opacity','0');
+        fields.eq(currentIndex).animate({
+          left: '0%',
+          opacity: 1
+        },500);
+      }); 
+    }
+  });
+
+  $('#arrowright4').on('click',function(){
+    if(targetIndex+1<=fields.length){
+      fields.eq(currentIndex).animate({
+        left: '-100%',
+        opacity: 0
+      },500,function(){
+          fields.eq(currentIndex).hide();
+          for(j=currentIndex+1 ; j<targetIndex ; j++){
+            fields.eq(j).css('left','0%');
+          }
+          currentIndex +=1;
+          targetIndex +=1;
+          fields.eq(targetIndex-1).show();
+          fields.eq(targetIndex-1).css('left','100%');
+          fields.eq(targetIndex-1).css('opacity','0');
+          fields.eq(targetIndex-1).animate({
+            left: '0%',
+            opacity: 1
+          },500);
+        }); 
+      }
+  });
 });
 
 
